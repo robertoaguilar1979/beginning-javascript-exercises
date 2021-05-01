68d6aa2e6a0f2aa63d22ec300a90080b509928ae
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2021-05-01 12:52:14 -0700

    fixed console.log print out

--- array/indexOf.js
+++ array/indexOf.js
@@ -4,22 +4,22 @@
 let names = ["robert", "angel", "karen"];
 
 //this adds charlie to the front on the names array.
-let name = names.unshift("charlie"); 
+let name = names.unshift("charlie");
 
-let findWord = function(strings){
+let findWord = function (strings) {
 
     let index = strings.indexOf("karen");
 
-    if (index > -1){
+    if (index > -1) {
 
-        strings.splice(index,1);
+        strings.splice(index, 1);
     }
-return index;
+    return index;
 
 }
 
 // this prints the array to see what we have in the array.
-console.log(`these are the names in the array ${names}`);
+console.log(`full array names: ${names}`);
 
 //this runs the function to find the indexOf karen in the array 
-console.log(`the index of kare is:${findWord(names)}`);
+console.log(`the index of karen is: ${findWord(names)}`);
