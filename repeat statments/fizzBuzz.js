a87a02430dcbe7168ffb347d88147a325a9a9a1c
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2021-05-02 23:42:12 -0700

    added fizzBuzz.js

---
+++ repeat statments/fizzBuzz.js
@@ -0,0 +1,18 @@
+/* Find multiples of 3 and 5*/
+/* if number is divisible by 3 use fizz*/
+/* if number is divisible by 5 use buzz*/
+/* divisible by 3 and 5 use fizzbuzz*/
+
+for (let i = 1; i <= 100; i++) {
+    let string = "";
+    if (i % 3 === 0) {
+        string += "fizz";
+    }
+    if (i % 5 === 0) {
+        string += "buzz"
+    }
+    if (string === "") {
+        string = i;
+    }
+    console.log(string);
+}
