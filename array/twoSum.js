2ba801743e38591b5e18c9df57befc4dc8e360f7
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2021-05-18 16:17:59 -0700

    this is a new algorithms that does a two sum of a given array.

---
+++ array/twoSum.js
@@ -0,0 +1,19 @@
+//two sum find two numbers that add to the target number. 
+// the number can only be used once 
+
+let numbers = [1, 5, 9];
+let target = 10;
+
+let sumNumbers = function (numbers, target) {
+    for (let i = 0; i < numbers.length - 1; i++) {
+        let firstNumber = numbers[i];
+        for (let j = i + 1; j < numbers.length; j++) {
+            let secondNumber = numbers[j];
+            if (firstNumber + secondNumber === target) {
+                return `index: ${[i]} with value: ${firstNumber} and index: ${[j]} with value: ${secondNumber}`;
+            }
+        }
+    }
+}
+
+console.log(sumNumbers(numbers, target));
