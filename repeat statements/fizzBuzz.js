14e1bd54d7874cbcb3f264a6c8b3737d69614171
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2021-05-19 15:40:38 -0700

    updated fizzBuzz

--- repeat statements/fizzBuzz.js
+++ repeat statements/fizzBuzz.js
@@ -3,16 +3,21 @@
 /* if number is divisible by 5 use buzz*/
 /* divisible by 3 and 5 use fizzbuzz*/
 
-for (let i = 1; i <= 100; i++) {
-    let string = "";
-    if (i % 3 === 0) {
-        string += "fizz";
+let fizzBuzz = function (number) {
+    for (let i = 1; i <= number; i++) {
+        let string = "";
+        if (i % 3 === 0) {
+            string += "fizz"
+        }
+        if (i % 5 === 0) {
+            string += "buzz"
+        }
+        if (string === "") {
+            string = i;
+        }
+        console.log(string);
     }
-    if (i % 5 === 0) {
-        string += "buzz";
-    }
-    if (string === "") {
-        string = i;
-    }
-    console.log(string);
+
 }
+
+fizzBuzz(100);
