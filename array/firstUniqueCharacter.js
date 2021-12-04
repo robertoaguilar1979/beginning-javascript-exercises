5d0d64cc80c72ebaa1271a2f262afa5b5c13f10f
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2021-12-04 13:30:14 -0800

    first occuring character in a string

--- array/firstUniqueCharacter.js
+++ array/firstUniqueCharacter.js
@@ -1,11 +1,11 @@
 let uniqueChar = function (string) {
-  for (let i = 0; i < string.length; i++) {
-    const char = string[i];
+    for (let i = 0; i < string.length; i++) {
+        const char = string[i];
 
-    if (string.indexOf(char) === string.lastIndexOf(char)) return i;
-  }
+        if (string.indexOf(char) === string.lastIndexOf(char)) return i;
+    }
 
-  return -1;
+    return -1;
 };
 
 console.log(uniqueChar("rroobbert"));
