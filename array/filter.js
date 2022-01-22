2f75e1f799764e8bcd6d6302f118895159cee784
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-01-21 23:26:52 -0800

    how to use the filter array method

---
+++ array/filter.js
@@ -0,0 +1,7 @@
+let numbers = [2, 4, 10, 11, 15];
+
+let numbersUnderTen = numbers.filter((number) => number <= 10);
+let numbersOverTen = numbers.filter((number) => number >= 10);
+
+console.log(numbersUnderTen);
+console.log(numbersOverTen);
