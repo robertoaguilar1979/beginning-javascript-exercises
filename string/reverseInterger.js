ffb2b1caa42405e8f3ccb63cc1cb7e070b6a161e
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-01-29 22:49:04 -0800

    added new reverse a interger into string folder

---
+++ string/reverseInterger.js
@@ -0,0 +1,9 @@
+//reverse an interger
+
+let reverseInterger = function (number) {
+  let reverse = number.toString().split("").reverse().join("");
+
+  return parseInt(reverse) * Math.sign(number);
+};
+
+console.log(reverseInterger(-12345));
