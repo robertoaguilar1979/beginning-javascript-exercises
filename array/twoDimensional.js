1d28317ff164a887d8c4123397838c40ded6b750
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-03-01 20:47:50 -0800

    added two dimensional array

---
+++ array/twoDimensional.js
@@ -0,0 +1,11 @@
+let multiArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+let getFive = multiArray[1][1];
+let getThree = multiArray[0][2];
+
+console.log(getFive);
+console.log(getThree);
