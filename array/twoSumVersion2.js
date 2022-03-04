dc570c6ea1f48e2f014582fd81c998b46665732d
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-03-03 21:28:56 -0800

    added documentation to file

--- array/twoSumVersion2.js
+++ array/twoSumVersion2.js
@@ -3,18 +3,17 @@
 let numbers = [1, 5, 10, 15, 9];
 let target = 10;
 let twoSum = function (array, target) {
-    let hash = {};
-    for (let i = 0; i < array.length; i++) {
-        let num = array[i];
-        let potentialMatch = target - num;
-        if (potentialMatch in hash) {
-            return [hash[potentialMatch], i];
-        } else {
-            hash[num] = i;
-        }
-       // console.log(Object.entries(hash));
+  let hash = {};
+  for (let i = 0; i < array.length; i++) {
+    let num = array[i];
+    let potentialMatch = target - num;
+    if (potentialMatch in hash) {
+      return [hash[potentialMatch], i];
+    } else {
+      hash[num] = i;
     }
-
-}
+    console.log(Object.entries(hash));
+  }
+};
 
 console.log(twoSum(numbers, target));
