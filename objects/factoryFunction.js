398b972a60a201a07ebed7e89647602df4993172
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-03-05 17:30:08 -0800

    added new file factory function and updated create array file

---
+++ objects/factoryFunction.js
@@ -0,0 +1,17 @@
+function createPerson(firstName, lastName, city, zipCode) {
+  return {
+    firstName,
+    lastName,
+    city,
+    zipCode,
+    hello() {
+      return `hello ${this.firstName} ${this.lastName}`;
+    },
+  };
+}
+
+let person1 = createPerson("robert", "aguilar", "los angeles", 90022);
+
+console.log(person1);
+console.log(person1.hello());
+console.log(person1.zipCode);
