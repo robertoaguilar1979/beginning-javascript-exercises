0b9c4d14db3be26e808eb7a9022c266298f819a8
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-03-29 20:57:36 -0700

    updated class encapsulation and changed file to typescript for private class to work

--- objects/classEncapsulation.js
+++ objects/classEncapsulation.js
@@ -1,22 +1,50 @@
 //class encapsulation
 
 class Person {
-    #
-    firstName;#
-    lastName;#
-    age;#
-    weight;
+  #firstName = "john";
+  #lastName = "doe";
+  #age;
+  #weight;
 
-    constructor(firstName, lastName, age, weight) {
-        this.#firstName = firstName;
-        this.#lastName = lastName;
-        this.#age = age;
-        this.#weight = weight;
-    }
+  constructor(firstName, lastName, age, weight) {
+    this.#firstName = firstName;
+    this.#lastName = lastName;
+    this.#age = age;
+    this.#weight = weight;
+  }
 
-    set firstName(firstName) {
-        this.#firstName;
-    }
+  set firstName(firstName) {
+    this.#firstName = firstName;
+  }
 
+  get firstName() {
+    return this.#firstName;
+  }
+  set lastName(lastName) {
+    this.#lastName = lastName;
+  }
 
+  get lastName() {
+    return this.#lastName;
+  }
+
+  set age(age) {
+    this.#age = age;
+  }
+  get age() {
+    return this.#age;
+  }
+  set weight(weight) {
+    this.#weight = weight;
+  }
+  get weight() {
+    return this.#weight;
+  }
 }
+
+let robert = new Person("robert", "aguilar", 42, 160);
+
+console.log(robert.firstName);
+console.log(robert.lastName);
+console.log(robert.age);
+console.log(robert.weight);
