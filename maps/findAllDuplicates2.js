3413eb72f8b72c86742b7b628b1f16076a0c1e39
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-04-19 21:07:24 -0700

    updated findAllDuplicates2

--- maps/findAllDuplicates2.js
+++ maps/findAllDuplicates2.js
@@ -1,20 +1,29 @@
-let names = ["robert", "angel", "nami", "karen", "neil", "frida", "zella", "zoren", "robert", "angel", "karen"];
+let names = [
+    "robert",
+    "angel",
+    "nami",
+    "karen",
+    "neil",
+    "frida",
+    "zella",
+    "zoren",
+    "robert",
+    "angel",
+    "karen",
+];
 
 let ifDouble = function (array) {
-    let double = new Map;
-    let single = new Map;
+    let double = new Map();
+    let single = new Map();
 
     for (const string of array) {
-
         if (single.has(string)) {
-            double.set(string, true)
+            double.set(string, true);
         } else {
-            single.set(string);
+            single.set(string, true);
         }
     }
     return double;
-
-}
-
+};
 
 console.log(ifDouble(names));
