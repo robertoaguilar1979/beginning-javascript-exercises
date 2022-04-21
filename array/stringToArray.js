6d44a7b5cbd1410f62f9e0edb96f28ec2a21319f
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-04-20 23:08:40 -0700

    added new string to array file

---
+++ array/stringToArray.js
@@ -0,0 +1,9 @@
+//this converts a string to an array.
+
+let string = "roberto arciniega aguilar";
+
+
+let stringToArray = string.split(" ");
+
+console.log(`this is a string: ${string}`);
+console.log(stringToArray);
