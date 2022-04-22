8095dc617257ab637a08da3ba876aa9ba881f80c
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-04-21 19:46:51 -0700

    added new file array to string to string folder

---
+++ string/arrayToString.js
@@ -0,0 +1,11 @@
+// this converts an array into a string
+
+
+
+let array = ["r", "o", "b", "e", "r", "t", " ", "a", "g", "u", "i", "l", "a", "r"];
+
+
+let arrayToString = array.join("");
+
+
+console.log(arrayToString);
